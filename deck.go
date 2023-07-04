c41dcb1721dc9bbdb445887c316916d328ba25e9
FdHerrera <soyfdherrera@gmail.com>
2023-07-04 12:15:32 -0300

    Type of cards with functionality to print every card and deal, multiple vals returned by a func?????

---
+++ deck.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardVals := []string{"Ace", "Two", "Three", "Four"}
+	for _, suit := range cardSuits {
+		for _, cardVal := range cardVals {
+			cards = append(cards, cardVal+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func (d deck) deal(handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	cards := newDeck()
+	hand, remainingCards := cards.deal(5)
+	hand.print()
+	remainingCards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
