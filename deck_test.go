4b46dc892627cc7010232d7e6660ae0f5bfe789a
FdHerrera <soyfdherrera@gmail.com>
2023-07-12 12:19:23 -0300

    Tests save to disk and load from disk

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -18,4 +19,28 @@ func TestCreateNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected last card to be Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestDeckSaveToFileAndNewDeckFromFile(t *testing.T) {
+	f := "_decktesting"
+	os.Remove(f)
+	d := newDeckForTesting()
+	d.saveToFile(f)
+	deckFromFile := newDeckFromFile(f)
+
+	if len(deckFromFile) != len(d) {
+		t.Errorf("Expected %v cards but got %v", len(deckFromFile), len(d))
+	}
+
+	for i := range d {
+		if d[i] != deckFromFile[i] {
+			t.Errorf("Found different cards at position : [%v], expected card [%v], found [%v]", i, d[i], deckFromFile[i])
+		}
+	}
+
+	os.Remove(f)
+}
+
+func newDeckForTesting() deck {
+	return deck{"SomeCard", "SomeOtherCard", "AndAnotherCard"}
 }
